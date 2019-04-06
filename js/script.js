0d286d04c1da64dce5efc6714837ddfcf9b8bdb4
Paweł Tuliński <pawel.tulinski@gmail.com>
2019-04-06 12:24:32 +0200

    Add map

--- js/script.js
+++ js/script.js
@@ -11,21 +11,30 @@ const elem = document.querySelector('.carousel');
     slides += Mustache.render(template, data[i]);
   }
   output.innerHTML = slides;
-})()
+})();
 
-let flkty = new Flickity( elem, {
+window.initMap = function() {
+  let map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: data[0].coords});
+  // The marker, positioned at Uluru
+  for (let i = 0; i < data.length; i++) {
+    let marker = new google.maps.Marker({ position: data[i].coords, map: map });
+  }
+}
+
+let flkty = new Flickity(elem, {
   // options
   cellAlign: 'left',
-    contain: true,
+  contain: true,
   hash: true,
   pageDots: false,
-})
+});
 
-button.addEventListener('click', function() {
-    flkty.select(0);
-})
+button.addEventListener('click', function () {
+  flkty.select(0);
+});
 
-flkty.on( 'scroll', function( progress ) {
-  progress = Math.max( 0, Math.min( 1, progress ) );
+flkty.on('scroll', function (progress) {
+  progress = Math.max(0, Math.min(1, progress));
   progressBar.style.width = progress * 100 + '%';
-})
+});
